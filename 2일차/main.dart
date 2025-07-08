a51c29ea162e9fcc6f2dd325b94d4d83ff7b3925
LownLy33 <billionwhale48@gmail.com>
2025-07-08 13:36:32 +0900

    이로운 퀴즈풀이

--- 2일차/main.dart
+++ 2일차/main.dart
@@ -1,6 +1,20 @@
 void main() {
-  bool a = 1 == 1;
-  bool b = 2 == 2;
+  // 오전 퀴즈풀이.
+  var a = 'banana';
 
-  print(a && b);
+  if (a == 'apple') {
+    print(true);
+  } else if (a == 'watermelon') {
+    print(true);
+  } else {
+    print(true);
+  }
+
+  // 오후 퀴즈 풀이
+  print("[13] 'for' statement.\n");
+  var number = 1;
+  var count = 1;
+  for (count = 1; count <= 3; count++) {
+    print("$number x $count = ${number * count}");
+  }
 }
